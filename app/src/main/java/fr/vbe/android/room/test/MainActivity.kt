9fdda6ad94db4f7f891df2e71d6a5857c21a3041
vincent <vbessuges@gmail.com>
2018-04-21 00:41:43 +0200

    First commit

---
+++ app/src/main/java/fr/vbe/android/room/test/MainActivity.kt
@@ -0,0 +1,16 @@
+package fr.vbe.android.room.test
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+//        Room
+
+
+    }
+}
